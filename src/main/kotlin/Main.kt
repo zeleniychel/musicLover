d0a94adaff5538c5d09c8de3f79182fa2dccb91d
Anton Kamnev AN44 <gish18@gmail.com>
2023-05-17 01:15:44 +0300

    regularCustomerDiscount change

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -2,7 +2,6 @@ fun main() {
     val isRegularCustomer = true
     val amount = 10500.0
     var discount = 0.0
-    val regularCustomerDiscount = amount * 0.01
 
     if (amount > 1000 && amount <= 10000) {
         discount = 100.0
@@ -10,7 +9,10 @@ fun main() {
         discount = amount * 0.05
     }
 
-    val result = if (isRegularCustomer === true) (amount - discount) - regularCustomerDiscount  else amount - discount
+    var result = amount - discount
+    val regularCustomerDiscount = result * 0.01
+    if (isRegularCustomer) result -= regularCustomerDiscount
+
 
     println("Стоимость покупки: $amount")
     println("Скидка: $discount")
